55fb3b725ccef4568cca8559d7ef5c06b913969d
mariam-egm <mariam.gerges014@gmail.com>
2021-09-24 19:19:58 +0200

    add new post screen to tab navigation

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -3,13 +3,23 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Settings from '../screens/Settings';
 import Home from '../screens/Home';
+import NewPost from '../screens/NewPost';
+import colors from '../constants/colors';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
 	return (
-		<Tab.Navigator>
+		<Tab.Navigator
+			screenOptions={{ 
+				headerShown: false, 
+				tabBarActiveTintColor: colors.primaryColor 
+			}}
+		>
 			<Tab.Screen name="Home" component={Home} />
+			<Tab.Screen name="NewPost" component={NewPost} 
+				options={{title:'New Post'}}
+			/>
 			<Tab.Screen name="Settings" component={Settings} />
 		</Tab.Navigator>
 	);
---
+++ src/screens/NewPost/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const NewPost = () => {
+  return (
+    <>
+			<Text>Hello, I am your NewPost!</Text>
+    </>
+  );
+}
+
+export default NewPost;
