dd4fe5d1e364a838280df3442de2a6950bfebc9a
mariam-egm <mariam.gerges014@gmail.com>
2021-09-24 13:36:46 +0200

    implement authstack navigator

---
+++ src/navigation/StackNavigator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from '../screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+const AuthStackNavigator = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Login" component={Login} />
+		</Stack.Navigator>
+	);
+}
+  
+export { AuthStackNavigator };
