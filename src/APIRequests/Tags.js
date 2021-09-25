ec9eea68400c992cd5c9de23beaa8e0c151ca83e
mariam-egm <mariam.gerges014@gmail.com>
2021-09-25 16:46:15 +0200

    add spaces

--- src/APIRequests/Tags.js
+++ src/APIRequests/Tags.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
-import {APP_ID, DUMMY_API_URL} from '@env';
+import { APP_ID, DUMMY_API_URL } from '@env';
 
 const getTags = () => {
 	const url = `${DUMMY_API_URL}/tag`;
 	const options = {
 		headers: {
-				'app-id': APP_ID
+			'app-id': APP_ID
 		}
 	}
+
 	return axios.get(url, options)
 }
 
