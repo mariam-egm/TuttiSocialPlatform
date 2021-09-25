ef67e8a96af0b0456714d9a7f08d5d31c7e28c5d
mariam-egm <mariam.gerges014@gmail.com>
2021-09-26 01:29:09 +0200

    add post context to home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, 
+{ 
+  useEffect, 
+  useState, 
+  useReducer, 
+  useMemo, 
+  useContext 
+} from 'react';
 import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 
 import { getTags } from '../../APIRequests/Tags';
 import { addIndex } from '../../utils/FlatListUtil';
+import { getPosts as getPostsRequest } from '../../APIRequests/Posts';
 import PostsList from '../../components/PostsList';
 import styles from './style';
+import PostsContext from '../../context/contexts/postContext';
+import postsReducer from '../../context/reducers/postsReducer';
+import {
+  getPostsAction,
+  getMorePostsAction
+} from '../../context/actions/postActions';
+import { BY_TAG } from '../../constants/getPostType';
 
 const Home = () => {
+  const initialState = {
+		posts: [],
+		isLoading: true
+	}
+	
+	const [state, dispatch] = useReducer(postsReducer, initialState);
+  console.log(state.posts, "home screen");
+  const postContext = useMemo(
+    () => ({
+      getPosts: (posts) => {
+        dispatch(getPostsAction(posts));
+      },
+			getMorePosts: (posts) => {
+				dispatch(getMorePostsAction(posts));
+			},
+      getPostSelector: () => { 
+        return state.posts
+      }  
+		}),[state]
+	);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.activeUsersContainer}>
-        <ActiveUsersDropDown />
+    <PostsContext.Provider value={postContext}>
+      <View style={styles.container}>
+        <View style={styles.activeUsersContainer}>
+          <ActiveUsersDropDown />
+        </View>
+        <View style={styles.tagsContainer}>
+          <Tags />
+        </View>
+        <View style={styles.postsContainer}>
+          <PostsList />
+        </View>
       </View>
-      <View style={styles.tagsContainer}>
-        <Tags />
-      </View>
-      <View style={styles.postsContainer}>
-        <PostsList />
-      </View>
-		</View>
+    </PostsContext.Provider>
   );
 }
 
@@ -35,6 +73,7 @@ const ActiveUsersDropDown = () => {
 
 const Tags = () => {
   const [tags, setTags] = useState([]);
+  const { getPosts } = useContext(PostsContext);
 
   useEffect(() => {
     getTags()
@@ -43,8 +82,18 @@ const Tags = () => {
   }, []);
 
   const renderItem = ({ item }) => (
-    <TagCard title={item.name} />
+    <TagCard title={item.name} onTagPress={() => onTagPress(item.name)} />
   );
+
+  const onTagPress = (tagName) => {
+    getPostsRequest({
+      pageNumber: 0,
+      getPostsType: BY_TAG,
+      id: tagName
+    })
+		.then(response => getPosts(response.data.data))
+		.catch(error => console.log("from home error", error))
+  }
 
   return (
     <>
@@ -59,9 +108,12 @@ const Tags = () => {
   )
 }
 
-const TagCard = ({title}) => {
+const TagCard = ({title, onTagPress}) => {
   return (
-    <TouchableOpacity style={styles.tagCardContainer}>
+    <TouchableOpacity 
+      style={styles.tagCardContainer}
+      onPress={onTagPress}
+    >
       <Text style={styles.tagName}>{title}</Text>
     </TouchableOpacity>
   )
