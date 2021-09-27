fed5be4ff751afaf49efbb95612315414b3f4852
mariam-egm <mariam.gerges014@gmail.com>
2021-09-27 23:37:55 +0200

    add progress bar to splash screen

--- android/app/src/main/java/com/tuttisocialplatform/SplashActivity.java
+++ android/app/src/main/java/com/tuttisocialplatform/SplashActivity.java
@@ -2,16 +2,31 @@ package com.tuttisocialplatform;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private ProgressBar mProgress;
 
-        Intent intent = new Intent(this, MainActivity.class);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Show the splash screen
+        setContentView(R.layout.launch_screen);
+        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
         startActivity(intent);
-        finish();
     }
 }
